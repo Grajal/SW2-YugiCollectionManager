79d3becac10d37d8e56f201f82f6a3488a97945a
idoceb00 <idoceb00@estudiantes.unileon.es>
2025-05-07 01:01:17 +0200

    Feat add user creation endpoint with input binding, hashing and DB save

--- backend/handlers/user_handler.go
+++ backend/handlers/user_handler.go
@@ -1,9 +1,34 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Grajal/SW2-YugiCollectionManager/backend/database"
+	"github.com/Grajal/SW2-YugiCollectionManager/backend/models"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func CreateUser(c *gin.Context) {
+	var user models.User
 
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	user.Password = string(hashedPassword)
+
+	if err := database.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"user": user})
 }
 
 func GetUserByName(c *gin.Context) {
