be26848b727dcf3142b9608a55a07907d7fcc15d
idoceb00 <idoceb00@estudiantes.unileon.es>
2025-05-06 22:41:24 +0200

    Feat add user endpoint routes and handler method signatures

---
+++ backend/handlers/user_handler.go
@@ -0,0 +1,15 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func CreateUser(c *gin.Context) {
+
+}
+
+func GetUserByName(c *gin.Context) {
+
+}
+
+func DeleteUser(c *gin.Context) {
+
+}
---
+++ backend/routes/router.go
@@ -0,0 +1,43 @@
+package routes
+
+import (
+	"github.com/Grajal/SW2-YugiCollectionManager/backend/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+// New initializes the Echo router and returns it
+// func New() *echo.Echo {
+// 	e := echo.New()
+
+// 	// Example route
+// 	e.GET("/", func(c echo.Context) error {
+// 		return c.String(http.StatusOK, "Hello from Echo!")
+// 	})
+
+// 	// You can group routes or import handler packages here
+// 	e.GET("/health", handlers.HealthHandler)
+// 	e.GET("/getNewCard", handlers.GetNewCard)
+
+// 	return e
+// }
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	api := r.Group("/api")
+	{
+		users := api.Group("/users")
+		{
+			users.POST("/", handlers.CreateUser)
+			users.GET("/{username}", handlers.GetUserByName)
+			users.DELETE("/{username}", handlers.DeleteUser)
+		}
+
+		cards := api.Group("/cards")
+		{
+			cards.GET("/getNewCard", handlers.GetNewCard)
+		}
+	}
+
+	return r
+}
