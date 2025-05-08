74d3223d781f0b4dbbcc5001c7a337515a07a001
idoceb00 <idoceb00@estudiantes.unileon.es>
2025-05-08 23:43:40 +0200

    feat: middleware to validate the session with jwt

---
+++ backend/middleware/auth.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/Grajal/SW2-YugiCollectionManager/backend/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			c.Abort()
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		claims, err := utils.ValidateToken(tokenStr)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", claims.UserID)
+		c.Next()
+	}
+}
