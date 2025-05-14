3ebd9f2e7a62f43023d9746b55fb8c51f19ae49e
idoceb00 <idoceb00@estudiantes.unileon.es>
2025-05-15 01:19:32 +0200

    feat: add handlers and services to delte a deck and request user's decks

--- backend/handlers/deck_handler.go
+++ backend/handlers/deck_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/Grajal/SW2-YugiCollectionManager/backend/services"
 	"github.com/gin-gonic/gin"
@@ -33,4 +34,43 @@ func CreateDeck(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, deck)
+}
+
+func GetUserDecks(c *gin.Context) {
+	userID, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "user ID not found in context"})
+		return
+	}
+
+	decks, err := services.GetDeckByUserID(userID.(uint))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to fetch decks"})
+		return
+	}
+
+	c.JSON(http.StatusOK, decks)
+}
+
+func DeleteDeck(c *gin.Context) {
+	userID := c.MustGet("user_id").(uint)
+	deckIDStr := c.Param("id")
+
+	deckID, err := strconv.ParseUint(deckIDStr, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid deck ID"})
+		return
+	}
+
+	err = services.DeleteDeck(uint(deckID), userID)
+	if err != nil {
+		if errors.Is(err, services.ErrDeckNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Deck not found"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete deck"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "deck deleted successfully"})
 }
--- backend/services/deck_service.go
+++ backend/services/deck_service.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/Grajal/SW2-YugiCollectionManager/backend/database"
 	"github.com/Grajal/SW2-YugiCollectionManager/backend/models"
+	"gorm.io/gorm"
 )
 
 var ErrDeckAlreadyExists = errors.New("deck with the same name already exists")
 var ErrMaximumNumberOfDecks = errors.New("maximum number of decks reached")
+var ErrDeckNotFound = errors.New("deck not found")
 
 func CreateDeck(userID uint, name, description string) (*models.Deck, error) {
 	var count int64
@@ -37,4 +39,27 @@ func CreateDeck(userID uint, name, description string) (*models.Deck, error) {
 	}
 
 	return &deck, nil
+}
+
+func GetDeckByUserID(userID uint) ([]models.Deck, error) {
+	var decks []models.Deck
+	err := database.DB.Where("user_id = ?", userID).Find(&decks).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch decks: %w", err)
+	}
+
+	return decks, nil
+}
+
+func DeleteDeck(deckID uint, userID uint) error {
+	var deck models.Deck
+	err := database.DB.Where("id = ? AND user_id = ?", deckID, userID).First(&deck).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return ErrDeckNotFound
+	}
+	if err != nil {
+		return err
+	}
+
+	return database.DB.Delete(&deck).Error
 }
